0232dd242cd505ef82bec4088f0af54ee40f558f
zhush <zhush_2005@163.com>
2018-03-19 04:29:30 +0800

    rpcclient implements;

---
+++ TopShooter/SvrTest/src/libs/yrpc/rpcclient.go
@@ -0,0 +1,68 @@
+package yrpc
+
+import (
+	"libs/log"
+	"net/rpc"
+	"sync"
+	"time"
+)
+
+type YClient struct {
+	conn       *rpc.Client
+	isRunning  bool
+	mutex      sync.Mutex
+	remoteAddr string
+}
+
+func NewYClient(addr string) *YClient {
+	client := &YClient{}
+	client.remoteAddr = addr
+	client.isRunning = false
+	client.init()
+	return client
+}
+
+func (self *YClient) init() {
+	go func() {
+		for {
+			var err error
+			self.mutex.Lock()
+			self.conn, err = rpc.DialHTTP("tcp", self.remoteAddr)
+			if err != nil {
+				log.Debug("cannot connect Manager(%s), error:%s, try after 1 second!!", self.remoteAddr, err.Error())
+				self.mutex.Unlock()
+				time.Sleep(1 * time.Second)
+			} else {
+				self.isRunning = true
+				self.mutex.Unlock()
+				break
+			}
+		}
+	}()
+}
+
+func (self *YClient) IsRunning() bool {
+	self.mutex.Lock()
+	defer self.mutex.Unlock()
+	return self.isRunning
+}
+
+func (self *YClient) SendMsg(method string, reqJson string) (bool, bool, string) {
+	if self.IsRunning() == false {
+		log.Debug("The YClient is not running, addr:%s", self.remoteAddr)
+		return false, false, ""
+	}
+	log.Debug("Enter  sendToServer")
+	arg := &ReqParam{}
+	arg.MethodName = method
+	arg.JsonContent = reqJson
+
+	reply := &RespParam{}
+	log.Debug("Ready Call RemoteCall")
+	err := self.conn.Call("YService.RomoteCall", arg, reply)
+	if err != nil {
+		log.Error("YService.RomoteCall rpc error:", err.Error())
+		return false, false, ""
+	}
+	return reply.Result, reply.HasResponse, reply.JsonContent
+}
--- TopShooter/SvrTest/src/servers/dbserver/app/allrpchandlers.go
+++ TopShooter/SvrTest/src/servers/dbserver/app/allrpchandlers.go
@@ -1,10 +1,39 @@
 package app
 
 import (
+	"libs/log"
 	"libs/yrpc"
 )
 
 func registerAllHandlers(service *yrpc.YService) {
-	//yrpc.RegisterMsgHandler(service, "BroadMessageToAllGame", BroadMessageToAllGame)
-	//yrpc.RegisterMsgHandler(service, "RegisterGameServer", RegisterGameServer)
+	//register the crud method of databases
+	yrpc.RegisterMsgHandler(service, "ReadTable", ReadTable)
+	yrpc.RegisterMsgHandler(service, "AddTable", AddTable)
+	yrpc.RegisterMsgHandler(service, "UpdateTable", UpdateTable)
+	yrpc.RegisterMsgHandler(service, "RemoveTable", RemoveTable)
+}
+
+func ReadTable(reqJsonContent string) (isOk bool, hasResponse bool, responseJson string) {
+	isOk = true
+	hasResponse = false
+
+	return
+}
+
+func AddTable(reqJsonContent string) (isOk bool, hasResponse bool, responseJson string) {
+	isOk = true
+	hasResponse = false
+	return
+}
+
+func UpdateTable(reqJsonContent string) (isOk bool, hasResponse bool, responseJson string) {
+	isOk = true
+	hasResponse = false
+	return
+}
+
+func RemoveTable(reqJsonContent string) (isOk bool, hasResponse bool, responseJson string) {
+	isOk = true
+	hasResponse = false
+	return
 }
