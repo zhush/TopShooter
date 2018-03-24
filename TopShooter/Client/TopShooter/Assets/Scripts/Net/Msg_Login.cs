495ac829c5c2697e843b6ee2770d4968a1b547e5
zhush <zhush_2005@163.com>
2018-03-24 12:11:38 +0800

    协议

---
+++ TopShooter/Client/TopShooter/Assets/Scripts/Net/Msg_Login.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using msg;
+using PureMVC.Patterns;
+
+namespace YFNet {
+    public class Msg_Login
+    {
+        public static void handle_SC_LoginResponse(object msgData)
+        {
+            msg.SC_LoginResponse rcvmsg = msgData as msg.SC_LoginResponse;
+			Debug.Log("Receive ,Result:" + rcvmsg.LoginResult + " RoleInfo:" + rcvmsg.PlayerBaseInfo);
+
+			LoginResult loginResult = new LoginResult ();
+			loginResult.LoginAccName = rcvmsg.AccName;
+			loginResult.Result = 0;
+			Facade.SendNotification (NotifycationConstant.LoginGameResponse, loginResult);
+
+        }
+    }
+}
