6b131da153323c7cb4201c610548079352c7933f
zhushanhong.bs <ZHS@8844028.com>
2018-03-26 12:54:57 +0800

    删除;

--- TopShooter/Client/TopShooter/Assets/Scripts/Common/Constant.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Constant  {
-	public static string ServerIp = "127.0.0.1";
-	public static int Port = 13079;
-}
