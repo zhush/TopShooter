ce7d47989cd6f7e62e3180c54eb96f7391b05a3a
zhushanhong.bs <ZHS@8844028.com>
2018-04-11 12:10:44 +0800

    添加批处理打开和关闭服务器的脚本;

---
+++ TopShooter/SvrTest/bin/startall.bat
@@ -0,0 +1,5 @@
+start dbserver.exe
+start manageserver.exe
+start gameserver.exe
+start loginserver.exe
+start gateserver.exe
---
+++ TopShooter/SvrTest/bin/stopall.bat
@@ -0,0 +1,5 @@
+taskkill /f /im dbserver.exe
+taskkill /f /im manageserver.exe
+taskkill /f /im gameserver.exe
+taskkill /f /im loginserver.exe
+taskkill /f /im gateserver.exe
